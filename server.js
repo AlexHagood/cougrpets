91a76c03c79d31e51820c9cb960c8a6f264b2f0b
alex <alexander.hagood@wsu.edu>
2024-03-08 18:18:45 -0800

    functionality is now pretty much exactly the normal website. still some errors if user manually navigates to a .html file or if no page specified

--- server.js
+++ server.js
@@ -6,25 +6,41 @@ path = require('path');
 
 const templatePath = path.join(__dirname, 'nodetemplate.html');
 const templateContent = fs.readFileSync(templatePath, 'utf-8');
-app.use(express.static('.'));
+app.use(express.static(('.')));
+
+
+const sidebarPath = path.join(__dirname,  'nodetemplate.html');
+const sidebarContent = fs.readFileSync(sidebarPath, 'utf8');
 
 app.use((req, res, next) => {
-    const sidebarPath = path.join(__dirname,  'nodetemplate.html');
-    const contentPath = path.join(__dirname, `${req.url.slice(1)}.html`);
-    console.log(req)
+
+    console.log("path = " + req.path)
+
+
+    const contentPath = path.join(__dirname, `${req.path}.html`);
   
     // Read HTML files
-    const sidebarContent = fs.readFileSync(sidebarPath, 'utf8');
-    const contentPageContent = fs.readFileSync(contentPath, 'utf8');
+    
+    const pageContent = fs.readFileSync(contentPath, 'utf8');
   
     // Combine sidebar and content
-    const combinedHTML = `${sidebarContent}${contentPageContent}`;
+    let finalHTML = "hmm"
+    console.log("first line = " + pageContent.trim().split('\n')[0])
+    if (pageContent.trim().split('\n')[0].trim() == "<!NOSIDEBAR>")
+    {
+        console.log("No siderbar page loaded.")
+        finalHTML = `${pageContent};`
+    }
+    else
+    {
+        finalHTML = `${sidebarContent}${pageContent};`
+    };
   
     // Send the combined HTML as the response
-    res.send(combinedHTML);
+    res.send(finalHTML);
   });
 
-app.use(express.static(__dirname));
+
 
 
 
