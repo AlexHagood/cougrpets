4c82b06760c4812c108de580a16dafcd92ac4095
alex <alexander.hagood@wsu.edu>
2024-03-08 17:46:25 -0800

    started templatifying pages

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 path = require('path');
+
+
+const templatePath = path.join(__dirname, 'nodetemplate.html');
+const templateContent = fs.readFileSync(templatePath, 'utf-8');
+app.use(express.static('.'));
+
+app.use((req, res, next) => {
+    const sidebarPath = path.join(__dirname,  'nodetemplate.html');
+    const contentPath = path.join(__dirname, `${req.url.slice(1)}.html`);
+    console.log(req)
+  
+    // Read HTML files
+    const sidebarContent = fs.readFileSync(sidebarPath, 'utf8');
+    const contentPageContent = fs.readFileSync(contentPath, 'utf8');
+  
+    // Combine sidebar and content
+    const combinedHTML = `${sidebarContent}${contentPageContent}`;
+  
+    // Send the combined HTML as the response
+    res.send(combinedHTML);
+  });
 
 app.use(express.static(__dirname));
 
-app.get('*', (req, res) => {
-
-    const filePath = path.join(__dirname, `${req.path}.html`);
-    console.log("trying to serve " + filePath)
-
-    res.sendFile(filePath, (err) => {
-        if (err) {
-            res.status(404).send('File not found');
-        }
-    });
-});
 
 
 app.listen(3000, () => {
