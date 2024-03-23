6eb5b2c0e7e7aad798ffdd9151b574a7daff2364
alex <alexander.hagood@wsu.edu>
2024-03-23 11:02:11 -0700

    Added favicon

--- server.js
+++ server.js
@@ -11,21 +11,22 @@ app.use(express.static(('.')));
 
 const sidebarPath = path.join(__dirname,  'nodetemplate.html');
 const sidebarContent = fs.readFileSync(sidebarPath, 'utf8');
+//load the sidebar straihgt into memory, since nearly every page will use it.
+
 
 app.use((req, res, next) => {
 
     console.log("path = " + req.path)
 
-
-    const contentPath = path.join(__dirname, `${req.path}.html`);
-  
-    // Read HTML files
-    
+    const contentPath = path.join(__dirname, `${req.path}.html`);    
     const pageContent = fs.readFileSync(contentPath, 'utf8');
+    //fetch the HTML requested and load it into memory
   
     // Combine sidebar and content
     let finalHTML = "hmm"
-    console.log("first line = " + pageContent.trim().split('\n')[0])
+
+
+    // if the first line of a page is <!NOSIDEBAR>, then our server won't add the sidebar code. Useful for the registration and login pages.
     if (pageContent.trim().split('\n')[0].trim() == "<!NOSIDEBAR>")
     {
         console.log("No siderbar page loaded.")
@@ -40,6 +41,10 @@ app.use((req, res, next) => {
     res.send(finalHTML);
   });
 
+
+app.use('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, 'favicon.ico'));
+})
 
 
 
