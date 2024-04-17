fdf950dbba108ce540ea6ffd0ef7722d1ed24646
alex <alexander.hagood@wsu.edu>
2024-04-17 16:28:27 -0700

    Rewrote authentication controller in a more readable format, using await instead of .then()

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -6,49 +6,50 @@ function errorHandler(res, error) {
 }
 
 async function register(req, res) {
+  try {
   const { username, password, password2 } = req.body;
-  //console.log("username="+username+" password="+password+" password2="+password2)
-  //console.log(req)
-  authModel
-    .checkUser(username)
-    .then((userExists) => {
-      if (userExists){
-        console.log(`Duplicate user ${username} attempted to register`); res.status(400).send("User already exists!");}
-      else {
-        authModel
-        // Both password fields are being passed together, so this takes only the first one
-          .registerUser(username, password[0])
-          .then(() => {
-            console.log(`New user ${username} registered sucessfully!`);
-            res.redirect("/inventory");
-          })
-          .catch((error) => {
-            errorHandler(res, error);
-          });
-      }
-    })
-    .catch((error) => {
-      errorHandler(res, error);
-    });
-}
+  const userExists = await authModel.checkUser(username)
+
+  if (userExists) {
+
+    console.log(`Duplicate user ${username} attempted to register`); res.status(400).send("User already exists!");
+
+  } else {
+
+    await authModel.registerUser(username, password[0]);
+    console.log(`New user ${username} registered sucessfully!`);
+    req.session.user = { username }
+    res.redirect("/inventory");
+
+  }} catch (error){
+
+    console.log("Error registering user");
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }}
 
 async function login() {
+  try{
     const { username, password} = req.body;
+
     console.log("Authenticating user "+username+" (check 1)")
-    authModel
-      .authenticateUser(username, password)
-      .then((validLogin) => {
-        if (validLogin){
-          console.log(`User ${username} logged in`); 
-          res.redirect("/inventory")
-        } else {
-            console.log(`User ${username} failed to log in`); 
-            res.status(401).send("Bad login!");
-        }
-      })
-      .catch((error) => {
-        errorHandler(res, error);
-      });
+
+    validLogin = await authModel.authenticateUser(username, password)
+
+    if (validLogin) {
+      console.log(`User ${username} logged in`); 
+      req.session.user = { username }
+      res.redirect("/inventory")
+    } else {
+      console.log(`User ${username} failed to log in`); 
+      res.status(401).send("Bad login!");}
+
+  } catch (error) {
+      console.log("Error logging in");
+      console.error(error);
+      res.status(500).send("Internal server error");
+
+    }
 }
 
 
