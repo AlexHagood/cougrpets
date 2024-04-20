e96861b1ee3503d1dfda35a50c7c6667c84eda0f
alex <alexander.hagood@wsu.edu>
2024-04-20 14:34:12 -0700

    Added username display on /profile

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,37 +2,44 @@ var express = require('express');
 var router = express.Router();
 const Profile = require('../models/Profile')
 
+// Alternate login implementation 
 
-router.post('/create', async function(req, res, next){
-    try{
-        await Profile.create(
-            {
-                username: req.body.username,
-                petname: req.body.petname,
-            }
-        )
-        res.redirect('/profile?msg=successful&username='+req.body.username)
-    } catch (error){
-        res.redirect('/?msg='+new URLSearchParams(error.toString()).toString()+'&username'+req.body.username)
-    }
-})
+// router.post('/create', async function(req, res, next){
+//     try{
+//         await Profile.create(
+//             {
+//                 username: req.body.username,
+//                 petname: req.body.petname,
+//             }
+//         )
+//         res.redirect('/profile?msg=successful&username='+req.body.username)
+//     } catch (error){
+//         res.redirect('/?msg='+new URLSearchParams(error.toString()).toString()+'&username'+req.body.username)
+//     }
+// })
 
-router.get('/login', async function(req, res, next){
-    const username = await Profile.findUser(req.params.username, req.params.petname);
-    if (username){
-        res.render('profile',{username})
-    }else{
-        res.redirect('/?msg=profile+not+found&?username='+req.params.username)
-    }
-})
+// router.get('/login', async function(req, res, next){
+//     const username = await Profile.findUser(req.params.username, req.params.petname);
+//     if (username){
+//         res.render('profile',{username})
+//     }else{
+//         res.redirect('/?msg=profile+not+found&?username='+req.params.username)
+//     }
+// })
 
-router.get('/', async function(req, res, next){
-    const profiles = await Profile.findAll();
-    if(req.query.msg){
-        res.locals.msg = req.query.msg;
-        res.locals.username = req.query.username;
-    }
-    res.render('profiles', {profiles});
+// router.get('/', async function(req, res, next){
+//     const profiles = await Profile.findAll();
+//     if(req.query.msg){
+//         res.locals.msg = req.query.msg;
+//         res.locals.username = req.query.username;
+//     }
+//     res.render('profiles', {profiles});
+// })
+
+router.get('/profile', (req, res) => {
+    console.log("profile router");
+    username = req.session.user.username
+    res.render("profile", {username : username})
 })
 
 module.exports = router
