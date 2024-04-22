2ebdeb965c7085b9ad64d31880e30042750f1673
alex <alexander.hagood@wsu.edu>
2024-04-21 22:44:20 -0700

    fixed crash on login

--- controllers/petStatus.js
+++ controllers/petStatus.js
@@ -7,8 +7,8 @@ const Money = require('../models/money')
 async function sideBarData (req, res, next){
     username = req.session.user.username;
     
-    await Pet.removePetFullness(username)
-    await Pet.removePetHappiness(username)
+    await Pet.removePetFullness(username, 1)
+    await Pet.removePetHappiness(username, 1)
     petHunger = await Pet.getPetFullness(username);
     petHappiness = await Pet.getPetHappiness(username);
     userMoney = await Money.getBalance(username);
